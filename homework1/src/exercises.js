41fda95310f1e44fbc79d8ea2cfbb7eccbcc0d1d
Bryan Moncada <bryan_moncada95@hotmail.com>
2020-05-24 00:09:30 -0700

    adding comments

--- homework1/src/exercises.js
+++ homework1/src/exercises.js
@@ -1,3 +1,10 @@
+/**
+ * Accepts a number of U.S cents and returns an array containing
+ * the smallest number of quarters, dimes, nickels and pennies.
+ * @param {number} amount the amount to be converted to coins
+ * @returns {array} array containing the respective number
+ * of quarters, dimes, nickels and pennies.
+ */
 export function change(amount) {
     if (amount < 0) {
         throw new RangeError('amount cannot be negative');
@@ -12,6 +19,13 @@ export function change(amount) {
     return resultArray;
 }
 
+/**
+ * Accepts a string and returns a new string equal to the initial
+ * string with all whitespace removed and ith character repeated
+ * i times.
+ * @param {string} stringToStretch String to be transformed
+ * @returns {string} stretched string
+ */
 export function stretched(stringToStretch) {
     const stringNoSpace = stringToStretch.replace(/\s/g, '');
     let stretchedString = "";
@@ -21,6 +35,12 @@ export function stretched(stringToStretch) {
     return stretchedString;
 }
 
+/**
+ * Randomly permutes a string. All possible permutations are
+ * equally likely.
+ * @param {string} stringToScramble string to be transformed
+ * @returns {string} string scrambled 
+ */
 export function scramble(stringToScramble) {
     const arrayString = Array.from(stringToScramble);
     let randomInt, temp;
@@ -34,6 +54,13 @@ export function scramble(stringToScramble) {
     return stringScrambled;
 }
 
+/**
+ * Yields successive powers of a base from 1 up to some limit.
+ * It consumes the values with a callback.
+ * @param {number} base power will be obtained from this number
+ * @param {number} limit maximum power of a base
+ * @param {function} callback asynchronous function
+ */
 export function powers(base, limit, callback) {
     if (limit <= 0) {
         return;
@@ -46,6 +73,12 @@ export function powers(base, limit, callback) {
     }
 }
 
+/**
+ * Generator function that yields successive powers of a base
+ * from 1 up to some limit
+ * @param {number} base power will be obtained from this number
+ * @param {number} limit maximum power of a base
+ */
 export function* powersGenerator(base, limit) {
     let value = 1;
     while (value <= limit) {
@@ -54,6 +87,11 @@ export function* powersGenerator(base, limit) {
     }
 }
 
+/**
+ * "Chainable" function that accepts one string per call.
+ * When called without arguments, returns the words previously
+ * passed, in order, separated by a single space.
+ */
 export function say(currentString) {
     if (currentString === undefined) {
         return '';
@@ -70,6 +108,13 @@ export function say(currentString) {
     }
 }
 
+/**
+ * Interleaves an array with other values. Extra elements should 
+ * end up at the end of the result.
+ * @param {array} array to be interleaved
+ * @param  {...any} otherParams values to be interleaved with array
+ * @returns {array} interleaved array
+ */
 export function interleave(array, ...otherParams) {
     const arrayOtherParams = [...otherParams];
     const interleavedArray = [];
@@ -99,6 +144,11 @@ export function interleave(array, ...otherParams) {
     return interleavedArray;
 }
 
+/**
+ * Encrypts and decrypts messages. Returns two functions in
+ * an array. The first one is an encrypt function, the second one
+ * a decrypt function.
+ */
 export function makeCryptoFunctions(key, algorithm, vector) {
     const crypto = require('crypto');
     const encrypt = (data) => {
