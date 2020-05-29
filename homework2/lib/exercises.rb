74e093d43e28c79c365743b967ec5d3cb506c55f
Bryan Moncada <bryan_moncada95@hotmail.com>
2020-05-28 23:40:46 -0700

    complete all functions

--- homework2/lib/exercises.rb
+++ homework2/lib/exercises.rb
@@ -12,7 +12,9 @@ end
 class String
     def stretched
         self.gsub!(/\s/, '')
-        (1..self.length).map(&->(x) {self[x-1] * x}).join('')
+        (1..self.length)
+            .map(&->(x) {self[x-1] * x})
+            .join('')
     end
 end
 
@@ -22,4 +24,62 @@ def scramble(string)
         string[randomInt], string[x] = string[x], string[randomInt]
     end
     string
+end
+
+def powers(base, limit)
+    value = 1
+    while value <= limit
+        yield value
+        value *= base
+    end
+end
+
+def powers_generator(base, limit)
+    value = 1
+    f = Fiber.new do
+        while value <= limit
+            Fiber.yield value
+            value *= base
+        end
+    end
+end
+
+def say
+    lambda {
+        |string = nil|
+        return '' if string == nil
+        return lambda {
+            |next_string = nil|
+            return string if next_string == nil
+            return say[string + ' ' + next_string]
+        }
+    }
+end
+
+class Array
+    def interleave(*values)
+        first_length = self.length
+        second_length = values.length
+        max_length = [first_length, second_length].max
+        results = []
+        (0...max_length).each do |x|
+            results << self[x] if x < first_length
+            results << values[x] if x < second_length
+        end
+        results
+    end
+end 
+
+def pokemon_sprites(pokemon)
+    require 'net/http'
+    require 'json'
+    url = "https://pokeapi.co/api/v2/pokemon/" + pokemon
+    uri = URI(url)
+    begin
+        response = Net::HTTP.get(uri)
+        JSON.parse(response)['sprites']
+    rescue
+        code_response = Net::HTTP.get_response(uri).code
+        raise JSON::ParserError, "API responded with " + code_response
+    end   
 end
--- homework2/test/test_exercises.rb
+++ homework2/test/test_exercises.rb
@@ -36,51 +36,51 @@ class TestExercises < Test::Unit::TestCase
     assert_equal(possibilities.size, 0)
   end
 
-#  def test_powers
-#    assert_equal(to_enum(:powers, 2, -5).to_a, [])
-#    assert_equal(to_enum(:powers, 7, 0).to_a, [])
-#    assert_equal(to_enum(:powers, 3, 1).to_a, [1])
-#    assert_equal(to_enum(:powers, 2, 63).to_a, [1, 2, 4, 8, 16, 32])
-#    assert_equal(to_enum(:powers, 2, 64).to_a, [1, 2, 4, 8, 16, 32, 64])
-#  end
-#
-#  def test_powers_generator
-#    g1 = powers_generator(2, 1)
-#    assert_equal(g1.resume, 1)
-#    assert_nil(g1.resume)
-#    assert_raise(FiberError) {g1.resume}
-#    g2 = powers_generator(3, 100)
-#    assert_equal(g2.resume, 1)
-#    assert_equal(g2.resume, 3)
-#    assert_equal(g2.resume, 9)
-#    assert_equal(g2.resume, 27)
-#    assert_equal(g2.resume, 81)
-#    assert_nil(g2.resume)
-#    assert_raise(FiberError) {g2.resume}
-#  end
-#
-#  def test_say
-#    assert_equal(say[], '')
-#    assert_equal(say['hi'][], 'hi')
-#    assert_equal(say['hi']['there'][], 'hi there')
-#    assert_equal(say['hello']['my']['name']['is']['Colette'][], 'hello my name is Colette')
-#  end
-#
-#  def test_interleave
-#    assert_equal([].interleave(), [])
-#    assert_equal([1, 4, 6].interleave(), [1, 4, 6])
-#    assert_equal([].interleave(2, 3), [2, 3])
-#    assert_equal([1].interleave(9), [1, 9])
-#    assert_equal([8, 8, 3, 9].interleave(1), [8, 1, 8, 3, 9])
-#    assert_equal([2].interleave(7, '8', {}), [2, 7, '8', {}])
-#  end
-#
-#  def test_pokemon_sprites
-#    ditto_sprites = pokemon_sprites('ditto')
-#    assert ditto_sprites.key?('front_default')
-#    assert ditto_sprites['front_shiny'].include?('sprites/pokemon/shiny/132.png')
-#    assert_raise_message('API responded with 404') do
-#      pokemon_sprites('fh7824gf827g273fg')
-#    end
-#  end
+  def test_powers
+    assert_equal(to_enum(:powers, 2, -5).to_a, [])
+    assert_equal(to_enum(:powers, 7, 0).to_a, [])
+    assert_equal(to_enum(:powers, 3, 1).to_a, [1])
+    assert_equal(to_enum(:powers, 2, 63).to_a, [1, 2, 4, 8, 16, 32])
+    assert_equal(to_enum(:powers, 2, 64).to_a, [1, 2, 4, 8, 16, 32, 64])
+  end
+
+  def test_powers_generator
+    g1 = powers_generator(2, 1)
+    assert_equal(g1.resume, 1)
+    assert_nil(g1.resume)
+    assert_raise(FiberError) {g1.resume}
+    g2 = powers_generator(3, 100)
+    assert_equal(g2.resume, 1)
+    assert_equal(g2.resume, 3)
+    assert_equal(g2.resume, 9)
+    assert_equal(g2.resume, 27)
+    assert_equal(g2.resume, 81)
+    assert_nil(g2.resume)
+    assert_raise(FiberError) {g2.resume}
+  end
+
+  def test_say
+    assert_equal(say[], '')
+    assert_equal(say['hi'][], 'hi')
+    assert_equal(say['hi']['there'][], 'hi there')
+    assert_equal(say['hello']['my']['name']['is']['Colette'][], 'hello my name is Colette')
+  end
+
+  def test_interleave
+    assert_equal([].interleave(), [])
+    assert_equal([1, 4, 6].interleave(), [1, 4, 6])
+    assert_equal([].interleave(2, 3), [2, 3])
+    assert_equal([1].interleave(9), [1, 9])
+    assert_equal([8, 8, 3, 9].interleave(1), [8, 1, 8, 3, 9])
+    assert_equal([2].interleave(7, '8', {}), [2, 7, '8', {}])
+  end
+
+  def test_pokemon_sprites
+    ditto_sprites = pokemon_sprites('ditto')
+    assert ditto_sprites.key?('front_default')
+    assert ditto_sprites['front_shiny'].include?('sprites/pokemon/shiny/132.png')
+    assert_raise_message('API responded with 404') do
+      pokemon_sprites('fh7824gf827g273fg')
+    end
+  end
 end
