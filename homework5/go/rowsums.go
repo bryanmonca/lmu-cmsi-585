070763743268a858a9e7430c60a428cf6fe24c31
Bryan Moncada <bryan_moncada95@hotmail.com>
2020-06-26 00:16:06 -0700

    adding rowsums solution

---
+++ homework5/go/rowsums.go
@@ -0,0 +1,39 @@
+package rowsums
+
+// Sums the numbers in a slice sending the sum to two different
+// channels depending on the row length.
+func sum(row []int, evens, odds chan int) {
+	sum := 0
+	for _, v := range row {
+		sum += v
+	}
+	if len(row) % 2 == 0 {
+		evens <- sum
+	} else {
+		odds <- sum
+	}
+}
+
+// Function that returns the sum of the rows in with even length and 
+// the sum of the rows with odd length. It uses a goroutine for each 
+// row sending the sum to two different channels depending on the 
+// length of each row.
+func RowSums(matrix [][]int) (int, int) {
+	evens := make(chan int)
+	odds := make(chan int)
+	for _, row := range matrix {
+		go sum(row, evens, odds)
+	}
+
+	evensSum := 0
+	oddsSum := 0
+	for i := 0; i < len(matrix); i++ {
+		select {
+		case receiveEven := <- evens:
+			evensSum += receiveEven
+		case receiveOdd := <- odds:
+			oddsSum += receiveOdd
+		}
+	}
+	return evensSum, oddsSum
+}
